11013bb8f78f32e9b508b67bee1eacea6593111b
Calebiin <greenbeancn@gmail.com>
2021-09-14 20:30:20 -0600

    Refactorizando en funciones para cada formula

--- Calculadora de la vida/Script.js
+++ Calculadora de la vida/Script.js
@@ -19,6 +19,33 @@ $scope.lista = [{
     dbaja: '25'
 }];
 
+$scope.resultados = [
+    {
+        nombre: "Maximin",
+        elemento: "",
+        valor: ""
+    },
+    {
+        nombre: "Maximax",
+        elemento: "",
+        valor: ""
+    },
+    {
+        nombre: "Coeficiente de optimismo-pesimismo",
+        elemento: "",
+        valor: ""
+    },
+    {
+        nombre: "Razón suficiente",
+        elemento: "",
+        valor: ""
+    },
+    {
+        nombre: "Minimax",
+        elemento: "",
+        valor: ""
+    }
+]
 
 $scope.eliminar = function(row) {
     if (confirm("¿Seguro que desea eliminar?")) {
@@ -35,42 +62,82 @@ $scope.agregar = function() {
     })
 };
 
-$scope.recuperarValores = function() {
-    var jsonDiv = document.getElementById("JSON");
-    jsonDiv.innerHTML = "";
-    console.log($scope.lista);
-    console.log($scope.lista[0].dalta.trimLeft().trimRight())
-    const maxDalta = 
-          $scope.lista.filter(element => element.dalta == Math.max.apply(Math, $scope.lista.map(function(o) { return o.dalta; })));
-    const minDbaja = $scope.lista.filter(element => element.dbaja == Math.min.apply(Math, $scope.lista.map(function(o) { return o.dbaja; })));
+function maximin(){
+    ///Elementos con mejor rendimiento en demanda baja
+    const data = $scope.lista.filter(element => element.dbaja == Math.max.apply(Math, $scope.lista.map(function(o) { return o.dbaja; })));
 
- 
-    //$("#JSON").text('Max: ' + JSON.stringify(maxDalta) + '\nMin: ' + JSON.stringify(minDbaja));
-    var tag = document.createElement("p");
-    var text = document.createTextNode(
-        "Max element(s): " + 
-        JSON.stringify(
-                maxDalta.map(function(x) {
-                return x.nombre;
-            })
-        ) 
-    );
-    tag.appendChild(text);
-    jsonDiv.appendChild(tag);
-    
-    tag = document.createElement("p");
-    text = document.createTextNode(
-        "Min element(s): " + 
-        JSON.stringify(
-                minDbaja.map(function(x) {
-                return x.nombre;
-            })
-        )
-    );
-    tag.appendChild(text);
-    jsonDiv.appendChild(tag);
- 
-   //$("#JSON").text('EL/los maximo(s) son: ' + maxDalta[0].nombre );
+    //Asigno el/los elemento(s) que cumple(n) con el criterio
+    $scope.resultados.find(element => element.nombre == "Maximin").resultado = data.map(function(x) {return x.nombre}).join(', ');
+
+    //Asigno el valor que tienen el/los elemento(s) correspondiente(s)
+    $scope.resultados.find(element => element.nombre == "Maximin").valor = data[0].dbaja;   
+};
+
+function maximax(){
+    ///Elementos con mejor rendimiento en demanda alta
+    const data = $scope.lista.filter(element => element.dalta == Math.max.apply(Math, $scope.lista.map(function(o) { return o.dalta; })));
+
+    //Asigno el/los elemento(s) que cumple(n) con el criterio
+    $scope.resultados.find(element => element.nombre == "Maximax").resultado = data.map(function(x) {return x.nombre}).join(', ');
+
+    //Asigno el valor que tienen el/los elemento(s) correspondiente(s)
+    $scope.resultados.find(element => element.nombre == "Maximax").valor = data[0].dalta;  
+}
+
+function coeficiente(){
+    ///Elementos con coeficiente de optimismo-pesimismo
+    const data = [
+        {
+            nombre: "un nombre",
+            coeficiente: 0
+        }
+    ]
+
+    //Asigno el/los elemento(s) que cumple(n) con el criterio
+    $scope.resultados.find(element => element.nombre == "Coeficiente de optimismo-pesimismo").resultado = data.map(function(x) {return x.nombre}).join(', ');
+
+    //Asigno el valor que tienen el/los elemento(s) correspondiente(s)
+    $scope.resultados.find(element => element.nombre == "Coeficiente de optimismo-pesimismo").valor = data[0].coeficiente;  
+}
+
+function razon(){
+    ///Promedio de todos los valores
+    const data = [
+        {
+            nombre: "un nombre",
+            razon: 0
+        }
+    ]
+
+    //Asigno el/los elemento(s) que cumple(n) con el criterio
+    $scope.resultados.find(element => element.nombre == "Razón suficiente").resultado = data.map(function(x) {return x.nombre}).join(', ');
+
+    //Asigno el valor que tienen el/los elemento(s) correspondiente(s)
+    $scope.resultados.find(element => element.nombre == "Razón suficiente").valor = data[0].razon;  
+}
+
+function minimax(){
+    ///Coste de oportunidad
+    const data = [
+        {
+            nombre: "un nombre",
+            minimax: 0
+        }
+    ]
+
+    //Asigno el/los elemento(s) que cumple(n) con el criterio
+    $scope.resultados.find(element => element.nombre == "Minimax").resultado = data.map(function(x) {return x.nombre}).join(', ');
+
+    //Asigno el valor que tienen el/los elemento(s) correspondiente(s)
+    $scope.resultados.find(element => element.nombre == "Minimax").valor = data[0].minimax;  
+}
+$scope.recuperarValores = function() {
+    //Llamo las funciones para calcular las fórmulas
+    maximin();
+    maximax();
+    //coeficiente();
+    //razon();
+    //minimax();
 };
 }]);
 
